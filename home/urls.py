7bc150d565f89156080c663803f42a617cbdc327
VithalGulati <53330675+VithalGulati@users.noreply.github.com>
2022-01-06 21:53:33 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -16,7 +16,8 @@ urlpatterns = [
     path("revenueprediction",views.revenueprediction,name='revenueprediction'),
 
     
-    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ]
 
