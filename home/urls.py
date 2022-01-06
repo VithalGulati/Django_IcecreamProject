d2370ad780945fea69a53568b48f8415cdcd336f
VithalGulati <53330675+VithalGulati@users.noreply.github.com>
2022-01-06 21:18:07 +0530

    new

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from home import views
+from django.views.static import serve
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path("",views.index,name='home'),
     path("home",views.index,name='home'),
@@ -16,3 +20,4 @@ urlpatterns = [
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ]
+
