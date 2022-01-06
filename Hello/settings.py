39377e3da0d42276effa7ffcb9657cdfb2f969db
VithalGulati <53330675+VithalGulati@users.noreply.github.com>
2022-01-06 21:05:36 +0530

    Update settings.py

--- Hello/settings.py
+++ Hello/settings.py
@@ -126,9 +126,7 @@ STATIC_URL = '/static/'
 #Added manually
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    ('assets', 'app-root/repo/wsgi/openshift/static'),
 ]
-STATIC_ROOT = "app-root/repo/wsgi/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
